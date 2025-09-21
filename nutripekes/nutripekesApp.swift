8187b3c981a35196f593ebb039f8371fb44ee30f
ItzelCovas <itzelcovas@gmail.com>
2025-09-21 17:55:12 -0600

    Initial Commit

---
+++ nutripekes/nutripekesApp.swift
@@ -0,0 +1,17 @@
+//
+//  nutripekesApp.swift
+//  nutripekes
+//
+//  Created by Itzel Covarrubias on 21/09/25.
+//
+
+import SwiftUI
+
+@main
+struct nutripekesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
